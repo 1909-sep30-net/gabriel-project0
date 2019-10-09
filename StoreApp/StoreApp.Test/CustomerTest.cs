92a80637d436c79e081ebb173f9f16af3ffdc45d
yetinight <gang@ucsd.edu>
2019-10-08 23:47:51 -0500

    Filled out Unit Testing skeleton for all models

---
+++ StoreApp/StoreApp.Test/CustomerTest.cs
@@ -0,0 +1,32 @@
+﻿using StoreApp.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StoreApp.Test
+{
+    public class CustomerTest
+    {
+        readonly Customer customer = new Customer("Billy","Bobman");
+
+        [Fact]
+        public void Name_EmptyValue_ThrowsException()
+        {
+
+        }
+
+        [Fact]
+        public void Name_NonSpaceSeparatedValue_ThrowsException()
+        {
+
+        }
+
+        [Fact]
+        public void Name_ManySpaceSeparatedValue_ThrowsException()
+        {
+
+        }
+    }
+
+}
---
+++ StoreApp/StoreApp.Test/LocationTest.cs
@@ -0,0 +1,31 @@
+﻿using StoreApp.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StoreApp.Test
+{
+    public class LocationTest
+    {
+        Location location = new Location();
+
+        [Fact]
+        public void Name_EmptyValue_ThrowsException()
+        {
+
+        }
+
+        [Fact]
+        public void Name_Null_ThrowsNullException()
+        {
+
+        }
+
+        [Fact]
+        public void Name_SomeValue_StoresName()
+        {
+
+        }
+    }
+}
